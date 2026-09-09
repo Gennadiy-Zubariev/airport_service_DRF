6a216d0cc0a48df9b815b3f691aec675bfcd2e34
Gennadiy-Zubariev <genada89@gmail.com>
2026-09-09 17:59:36 +0300

    add models

---
+++ airport/utils/helpers.py
@@ -0,0 +1,8 @@
+import pathlib
+import uuid
+from django.utils.text import slugify
+
+
+def airplane_image_path(instance, filename):
+    filename = f"{ slugify(instance.info)}-{uuid.uuid4()}" + pathlib.Path(filename).suffix
+    return pathlib.Path("upload/buses") / pathlib.Path(filename)
